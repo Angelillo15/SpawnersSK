5003345b75741734f40fc3ec9e592edd7f71d580
Angelillo15 <contact@angelillo15.es>
2023-02-10 17:18:09 +0100

    Added constants to the plugin to get info like Commit or version

--- SpawnersSK-API/build.gradle.kts
+++ SpawnersSK-API/build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     id("java")
+    id("net.kyori.blossom") version "1.3.1"
+    id("org.ajoberstar.grgit") version "4.1.0"
 }
 
 group = "es.angelillo15.spsk"
-version = "1.0-SNAPSHOT"
+version = parent?.version ?: "undefined"
 
 repositories {
     mavenCentral()
@@ -11,4 +13,11 @@ repositories {
 
 dependencies {
     compileOnly("org.spigotmc:spigot-api:1.19-R0.1-SNAPSHOT")
+    compileOnly("net.byteflux:libby-bukkit:1.1.5")
+}
+
+blossom {
+    replaceTokenIn("src/main/java/es/angelillo15/spsk/api/Constants.java")
+    replaceToken("{version}", project.version)
+    replaceToken("{git-commit}",  grgit.head().abbreviatedId ?: "undefined")
 }
---
+++ SpawnersSK-API/src/main/java/es/angelillo15/spsk/api/Constants.java
@@ -0,0 +1,9 @@
+package es.angelillo15.spsk.api;
+
+public class Constants {
+    public static final String VERSION = "{version}";
+
+    public static final String COMMIT = "{git-commit}";
+
+    private Constants() {}
+}
