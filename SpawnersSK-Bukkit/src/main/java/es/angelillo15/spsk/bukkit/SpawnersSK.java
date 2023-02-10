71462a9f122e8871354fa94ecdf104169d7d5904
Angelillo15 <contact@angelillo15.es>
2023-02-10 16:33:40 +0100

    Added the drawLogo

--- SpawnersSK-Bukkit/src/main/java/es/angelillo15/spsk/bukkit/SpawnersSK.java
+++ SpawnersSK-Bukkit/src/main/java/es/angelillo15/spsk/bukkit/SpawnersSK.java
@@ -2,16 +2,40 @@ package es.angelillo15.spsk.bukkit;
 
 import es.angelillo15.spsk.api.ISpawnersSK;
 import es.angelillo15.spsk.api.utils.ILogger;
+import es.angelillo15.spsk.api.utils.TextUtils;
+import es.angelillo15.spsk.bukkit.utils.Logger;
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
-
 public class SpawnersSK extends JavaPlugin implements ISpawnersSK {
+    @Getter
+    private static SpawnersSK instance;
+    private boolean debug = false;
+    private ILogger logger;
+
+    @Override
+    public boolean isDebug() {
+        return debug;
+    }
+
     @Override
     public ILogger getPLogger() {
-        return null;
+        return this.logger;
     }
 
     @Override
     public void drawLogo() {
+        instance = this;
+        this.logger = new Logger(this.getLogger());
+        logger.info(TextUtils.simpleColorize("&b _____ _____ _____ _____"));
+        logger.info(TextUtils.simpleColorize("&b|   __|  _  |   __|  |  |"));
+        logger.info(TextUtils.simpleColorize("&b|__   |   __|__   |    -|"));
+        logger.info(TextUtils.simpleColorize("&b|_____|__|  |_____|__|__|"));
+        logger.info(TextUtils.simpleColorize(""));
+        logger.info(TextUtils.simpleColorize("Loading SpawnersSK v"
+                + this.getDescription().getVersion() + " by " +
+                this.getDescription().getAuthors().get(1))
+        );
+        logger.info(TextUtils.simpleColorize(""));
 
     }
 }
