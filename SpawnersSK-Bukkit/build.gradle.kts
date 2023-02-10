80fcfca7d96e4a8201b10d26fe8f20df4f4941db
Angelillo15 <contact@angelillo15.es>
2023-02-10 17:18:41 +0100

    Added new libraries

--- SpawnersSK-Bukkit/build.gradle.kts
+++ SpawnersSK-Bukkit/build.gradle.kts
@@ -12,6 +12,15 @@ repositories {
 dependencies {
     compileOnly("org.spigotmc:spigot-api:1.19-R0.1-SNAPSHOT")
     compileOnly(project(":SpawnersSK-API"))
+    compileOnly("com.github.Carleslc.Simple-YAML:Simple-Yaml:1.8.3")
+    compileOnly("com.github.Angelillo15:ConfigManager:1.4")
+    compileOnly("com.github.hamza-cskn.obliviate-invs:core:4.1.10")
+    compileOnly("com.github.hamza-cskn.obliviate-invs:advancedslot:4.1.10")
+    compileOnly("com.github.hamza-cskn.obliviate-invs:pagination:4.1.10")
+    compileOnly("com.github.hamza-cskn.obliviate-invs:configurablegui:4.1.10")
+    compileOnly("com.google.code.gson:gson:2.10.1")
+    compileOnly("net.byteflux:libby-bukkit:1.1.5")
+    compileOnly("ru.vyarus:yaml-config-updater:1.4.2")
 }
 
 tasks.processResources {
